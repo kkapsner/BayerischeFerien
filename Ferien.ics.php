1ab3fa122b32d911ca8cd26d2457e21faac31a4b
kkapsner <github@mail.kkapsner.de>
2022-08-03 22:12:46 +0200

    Fix validation errors

--- Ferien.ics.php
+++ Ferien.ics.php
@@ -2,9 +2,13 @@
 $calendar = new Calendar();
 CalendarTimezone::addGermanTimezone($calendar);
 
-eachFerien(function($name, $startDate, $endDate) use ($calendar){
+$nowString = (new DateTime())->format("Ymd\THis");
+
+eachFerien(function($name, $startDate, $endDate) use ($calendar, $nowString){
 	$event = new CalendarEvent();
 	$event->uid = preg_replace("/[^0-9a-z]/i", "_", $name) . "@kkapsner.de";
+	$event->dtstamp = $nowString;
+	$event->dtstamp->tzid = "Europe/Berlin";
 	$event->dtstart = $startDate->format("Ymd");
 	$event->dtstart->VALUE = "DATE";
 	$event->dtstart->tzid = "Europe/Berlin";
